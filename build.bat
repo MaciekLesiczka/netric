5efdc9453cd3d2629f17282eda5e0ade860796b9
macieklesiczka <maciek.lesiczka@gmail.com>
2015-11-24 19:05:33 +0100

    adjust fake path

--- build.bat
+++ build.bat
@@ -1,3 +1,3 @@
 @echo off
-::".nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "packages" "-ExcludeVersion"
+"src\.nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "src\packages" "-ExcludeVersion"
 "src\packages\FAKE\tools\Fake.exe" build.fsx
