2c0da0b3c616068b4134852e483c627acf216b01
macieklesiczka <maciek.lesiczka@gmail.com>
2016-06-19 19:19:54 +0200

    add conditonal fake install

--- build.bat
+++ build.bat
@@ -1,3 +1,7 @@
 @echo off
-"src\.nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "src\packages" "-ExcludeVersion"
-"src\packages\FAKE\tools\Fake.exe" build.fsx
+
+if not exist "src\packages\FAKE\tools\Fake.exe" (
+    "src\.nuget\NuGet.exe" "Install" "FAKE" "-OutputDirectory" "src\packages" "-ExcludeVersion" "-Prerelease"
+)
+
+"src\packages\FAKE\tools\Fake.exe" "build.fsx"
